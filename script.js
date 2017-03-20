4056916e81a21889f0edbfc80136dbb6a48d3375
joxterboi <joa123kim456@gmail.com>
2017-03-20 17:55:52 +0100

    Fixed header IMG scaling on mobile

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function bildSpel() {
   document.getElementById("secondPic").style.display = "none";
-  console.log("1");
   setTimeout(function inital() {
     document.getElementById("secondPic").style.display = "inline-block";
-    console.log("2");
     }, 5000);
 }
 setInterval(bildSpel, 10000);
